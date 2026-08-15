6d10c700d6bb464db19ef20a7729091e3f815094
Divya Darshini V <divyadharshiniv06@gmail.com>
2026-08-15 09:38:49 +0530

    Initial commit

---
+++ EvenOrOdd.java
@@ -0,0 +1,18 @@
+class EvenOrOdd {
+    static boolean isEven(int n) {
+        if(n%2==0){
+             return true;
+        }
+        else{
+            return false;
+        }
+         
+    }   
+    public static void main(String[]args){
+        System.out.println(isEven(16));
+         System.out.println(isEven(11));
+      
+    }
+   
+}
+       
